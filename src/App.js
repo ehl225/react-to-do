8161859aee25ed9a0e9686f1f317a32000555fc1
Erica <ehlam225@gmail.com>
2019-05-27 16:36:53 -0400

    Added descriptions and checkboxes in ToDo list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import './App.css';
-import ToDo from './components/ToDo.js';
-class App extends Component {
-  
-return (
-
 import React, { Component } from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+	this.state = {
+		todos: [
+			{ description: 'Finish homework', isCompleted: true
+},
+			{ description: 'Buy groceries', isCompleted: false
+},
+			{ description: 'Cook dinner', isCompleted: false }
+			]
+};
+}
   render () {
     return (
 
     <div className="App">
 	<ul>
-		<ToDo />
-		<ToDo />
+	{ this.state.todos.map((todo, index) =>
+		<ToDo key={index} description={todo.description}
+isCompleted={todo.isCompleted} />
+	)}
 	</ul>
     </div>
   );
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,26 +1,17 @@
-<<<<<<< HEAD
-import React, {Component} from 'react';
-
-class ToDo extends Component {
-	render() {
-		return (
-			<li> A todo will go here </li>
-		);
-	}
-}
-
-export default Todo;
-=======
 import React, { Component } from 'react';
 
 class ToDo extends Component {
   render() {
 	return (
-		<li> A todo will go here </li>
+		<li>
+			<input type="checkbox"
+checked={this.props.isCompleted} />
+			<span>{this.props.description} </span>
+		</li>
 	);
   }
 }
 
 export default ToDo;
->>>>>>> components
+
 
